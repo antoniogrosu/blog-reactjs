c7e8395c7336ddccb99d6d1f4855bab4195632f9
Antonio <antonio.webdesign19@gmail.com>
2023-02-20 00:14:34 +0200

    post component

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,10 +1,10 @@
 export default function Post(props) {
   const content = props.content.map((item) => {
-    return <p>{item}</p>;
+    return <p className="my-8 lato font-semibold text-md text-brown">{item}</p>;
   });
   return (
     <div>
-      <h1>{props.title}</h1>
+      <h1 className="text-3xl lato text-brown font-semibold">{props.title}</h1>
       {content}
     </div>
   );
