de241f238e4ffc2ac5c73ec3be437fab0dc4b278
Antonio <antonio.webdesign19@gmail.com>
2023-02-22 20:13:58 +0200

    try again deployment

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,23 +1,10 @@
 import Hero from "../Components/Hero";
 import Card from "../Components/Card";
 import { Link } from "react-router-dom";
-import { db } from "/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import DidYouKnow from "../Components/DidYouKnow";
 
 export default function HomePage() {
-  const [posts, setPosts] = useState([]);
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const postsCollectionRef = collection(db, "firstPage");
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getPosts();
-    setCount((num) => posts.length - 1);
-  }, []);
   return (
     <>
       <Hero />
